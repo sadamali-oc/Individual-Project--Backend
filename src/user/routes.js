9a65b037e9c47ce48edbfa3d876641833d47a83b
Chamalka Obadage <sadamalioc.21@uom.lk>
2025-02-09 19:04:01 +0530

    Initial commit

---
+++ src/user/routes.js
@@ -0,0 +1,13 @@
+const {Router} = require('express');
+const controller = require('./controller');
+
+const router = Router();
+
+router.get('/', controller.getUsers);
+   
+
+
+
+
+
+module.exports = router;

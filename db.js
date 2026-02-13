4adfa2b1dde1f11655645dc3cd2b721f651967d8
Chamalka Obadage <sadamalioc.21@uom.lk>
2026-02-13 20:58:33 +0530

    credential management

--- db.js
+++ db.js
@@ -1,11 +1,27 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
+// require("dotenv").config();
+//  const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "event_db",
+//   password: "2001",
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
+
+const { Pool } = require("pg");
 require("dotenv").config();
- const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "event_db",
-  password: "2001",
-  port: 5432,
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
+
